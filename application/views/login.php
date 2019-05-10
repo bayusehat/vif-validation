7a5d6dc28260d1626f7a7166e6960fc70a0f73ae
Arya Bayu <aryabayu23@gmail.com>
2019-05-10 23:53:03 +0700

    change style

--- application/views/login.php
+++ application/views/login.php
@@ -1,25 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<title>Login V1</title>
+	<title>Vif Validation Test Login</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
+	
 	<link rel="icon" type="image/png" href="<?= base_url();?>assets/images/icons/favicon.ico"/>
-<!--===============================================================================================-->
+
 	<link rel="stylesheet" type="text/css" href="<?= base_url();?>assets/vendor/bootstrap/css/bootstrap.min.css">
-<!--===============================================================================================-->
+
 	<link rel="stylesheet" type="text/css" href="<?= base_url();?>assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css">
-<!--===============================================================================================-->
+
 	<link rel="stylesheet" type="text/css" href="<?= base_url();?>assets/vendor/animate/animate.css">
-<!--===============================================================================================-->	
+	
 	<link rel="stylesheet" type="text/css" href="<?= base_url();?>assets/vendor/css-hamburgers/hamburgers.min.css">
-<!--===============================================================================================-->
+
 	<link rel="stylesheet" type="text/css" href="<?= base_url();?>assets/vendor/select2/select2.min.css">
-<!--===============================================================================================-->
+
 	<link rel="stylesheet" type="text/css" href="<?= base_url();?>assets/css/util.css">
 	<link rel="stylesheet" type="text/css" href="<?= base_url();?>assets/css/main.css">
-<!--===============================================================================================-->
+
 </head>
 <body>
 	
@@ -32,7 +32,7 @@
 
 				<form class="login100-form validate-form" method="post" id="loginForm">
 					<span class="login100-form-title">
-						Member Login
+						Validation App Login
 					</span>
 
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
