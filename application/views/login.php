7988e20a85593abdb664e94a51b5e1b3cc1356fb
Arya Bayu <aryabayu23@gmail.com>
2019-05-19 22:08:31 +0700

    login view & validate

--- application/views/login.php
+++ application/views/login.php
@@ -18,14 +18,13 @@
 	<div class="row">
 		<div class="col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-4 col-md-offset-4">
 			<div class="login-panel panel panel-default">
-				<div class="panel-heading">
-					Validation App Login
-				</div>
+				<center><img src="<?= base_url();?>assets/images/vif.png" class="img-responsive"></center>
+				
 				<div class="panel-body">
 					<form role="form" method="post">
 						<fieldset>
 							<div class="form-group">
-								<input class="form-control" placeholder="E-mail" name="email" id="email" autofocus="">
+								<input class="form-control" placeholder="E-mail" name="email" id="email" autofocus="" type="email">
 							</div>
 							<div class="form-group">
 								<input class="form-control" placeholder="Password" name="password" id="password" type="password">
@@ -80,7 +79,11 @@
 			var email  = $("#email").val();
 			var password  = $("#password").val();
 			var url = "<?php echo base_url('login');?>";
-
+			
+			if(checkEmail(email) == false){
+				swal_failed('Email format is invalid');
+				$("#email").focus();
+			}else{
 				$.ajax({
 					type : "POST",
 					url : "<?php echo base_url('login/login_user');?>",
@@ -107,7 +110,18 @@
 						$("#username").focus();
 					}
 				});
+			}
+				
 			});
+
+		function checkEmail(email) {
+			var regex = /^([a-zA-Z0-9_\.\-\+])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+			  if(!regex.test(email)) {
+			    return false;
+			  }else{
+			    return true;
+  			}
+		}
 	</script>
 
 </body>
