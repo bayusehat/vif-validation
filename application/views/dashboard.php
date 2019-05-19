a14d28210e9d19acbf92be8641c8f6c0bbde72ed
Arya Bayu <aryabayu23@gmail.com>
2019-05-19 23:04:21 +0700

    Fixing

--- application/views/dashboard.php
+++ application/views/dashboard.php
@@ -2,7 +2,7 @@
 		<div class="row">
 			<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
 				<div class="panel panel-teal panel-widget border-right">
-					<div class="row no-padding"><em class="fa fa-xl fa-file color-green"></em>
+					<div class="row no-padding"><em class="fa fa-xl fa-file color-blue"></em>
 						<div class="large">120</div>
 							<div class="text-muted">Form</div>
 						</div>
@@ -10,7 +10,7 @@
 				</div>
 				<div class="col-xs-6 col-md-3 col-lg-3 no-padding">
 					<div class="panel panel-blue panel-widget border-right">
-						<div class="row no-padding"><em class="fa fa-xl fa-wpforms color-blue"></em>
+						<div class="row no-padding"><em class="fa fa-xl fa-wpforms color-green"></em>
 							<div class="large">52</div>
 							<div class="text-muted">Form Active</div>
 						</div>
@@ -34,7 +34,7 @@
 				</div>
 			</div><!--/.row-->
 		</div>
-	
+
 	<div class="panel panel-container">
 		<div class="row">
 			<div class="col-md-6 col-sm-12 no-padding">
--- application/views/layout.php
+++ application/views/layout.php
@@ -137,7 +137,7 @@
 					</a></li>
 				</ul>
 			</li> -->
-			<li><a href="<?= base_url();?>login/logout" class="text-danger"><em class="fa fa-power-off">&nbsp;</em> Logout</a></li>
+			<li><a href="<?= base_url();?>login/logout" style="color:red"><em class="fa fa-power-off">&nbsp;</em> Logout</a></li>
 		</ul>
 	</div><!--/.sidebar-->
 		
